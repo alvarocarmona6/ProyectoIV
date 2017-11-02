525ddd2e8a97097a635f1ca7e74bb910b7e216ab
alvarocarmona6 <alvaro.carmona96@gmail.com>
2017-11-02 17:40:31 +0100

    Actualizo funciones

--- bot/funciones.py
+++ bot/funciones.py
@@ -1,3 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+import html5lib
+import re
 
 
 lista_jugadores_puntos = {'Curry': 25 , 'Lebron' : 15, 'Harden': 10}
@@ -20,4 +24,34 @@ def ObtenerClasificacion():
 
 
 
+        
 
+
+
+def Clasificacion():
+        req = requests.get('http://www.20minutos.es/deportes/estadisticas/nba/standings_conference.asp')
+
+        soup = BeautifulSoup(req.text, "html5lib")
+
+        puesto = soup.find_all('td', {'class' : 'shs1stCol shsNamD'})
+
+
+        patron = re.compile('</span></span>(.*?)</a>')
+
+        nombres= patron.findall(str(puesto))
+
+        mensaje = ""
+        mensaje = mensaje +"\nConferencia Este:\n"
+        for x in range(len(nombres)):
+                if(x == 15):
+                        mensaje = mensaje +"\nConferencia Oeste:\n"
+                        mensaje = mensaje +  "\nEn el puesto " + str(x-14) + " esta "+ nombres[x]
+                elif(x>15):
+                        mensaje = mensaje + "\nEn el puesto " + str(x-14) + " esta "+ nombres[x]
+
+                else:
+                        mensaje = mensaje +  "\nEn el puesto " + str(x+1) + " esta "+ nombres[x]
+
+        return mensaje
+
+	
