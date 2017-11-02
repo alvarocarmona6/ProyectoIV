f1dadcdb9229acf71689225f4623d0176055e29a
alvarocarmona6 <alvaro.carmona96@gmail.com>
2017-11-02 18:01:12 +0100

    Incorporo al bot /hola

--- bot/NBAbot.py
+++ bot/NBAbot.py
@@ -1,20 +1,31 @@
 import telebot 
 from telebot import types 
 from telebot import util
-import time # Libreria para hacer que el programa que controla el bot no se acabe.
+import time 
 import funciones as fun
 import os
 
 TOKEN = os.environ['TOKEN']
 bot = telebot.TeleBot(TOKEN)
 
-def listener(messages): # Con esto, estamos definiendo una funcion llamada 'listener', que recibe como parametro un dato llamado 'messages'.
-	for m in messages: # Por cada dato 'm' en el dato 'messages'
-        	if m.content_type == 'text': # Filtramos mensajes que sean tipo texto.
-			cid = m.chat.id # Almacenaremos el ID de la conversacion.
-		    	print ("[" + str(cid) + "]: " + m.text) # Y haremos que imprima algo parecido a esto -> [52033876]: /start
+def listener(messages): 
+	for m in messages: 
+        	if m.content_type == 'text': 
+			cid = m.chat.id 
+		    	print ("[" + str(cid) + "]: " + m.text) 
 
 bot.set_update_listener(listener)
+
+@bot.message_handler(commands=['hola']) 
+def clasificacion(m): #
+	
+	cid = m.chat.id # Guardamos el ID de la conversacion para poder responder.
+	mensaje= "Hola este bot te informará sobre la NBA, escribe /clasificacion para ver la clasificacion actual de la NBA, gracias :) "
+	bot.send_message( cid, mensaje)
+
+
+bot.set_update_listener(listener)
+
 
 @bot.message_handler(commands=['clasificacion']) 
 def clasificacion(m): # Definimos una funcion que resuelva lo que necesitemos.
