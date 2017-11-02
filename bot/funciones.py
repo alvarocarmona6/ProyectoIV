ab08c643f4173bb6bda08e319fb502b765976f7e
alvarocarmona6 <alvaro.carmona96@gmail.com>
2017-11-02 18:00:15 +0100

    Actualizo la funcion Clasificacion

--- bot/funciones.py
+++ bot/funciones.py
@@ -44,13 +44,13 @@ def Clasificacion():
         mensaje = mensaje +"\nConferencia Este:\n"
         for x in range(len(nombres)):
                 if(x == 15):
-                        mensaje = mensaje +"\nConferencia Oeste:\n"
-                        mensaje = mensaje +  "\nEn el puesto " + str(x-14) + " esta "+ nombres[x]
+                        mensaje = mensaje +"\n\nConferencia Oeste:\n"
+                        mensaje = mensaje +  "\nEn el puesto " + str(x-14) + " " + nombres[x]
                 elif(x>15):
-                        mensaje = mensaje + "\nEn el puesto " + str(x-14) + " esta "+ nombres[x]
+                        mensaje = mensaje + "\nEn el puesto " + str(x-14) + " " +  nombres[x]
 
                 else:
-                        mensaje = mensaje +  "\nEn el puesto " + str(x+1) + " esta "+ nombres[x]
+                        mensaje = mensaje +  "\nEn el puesto " + str(x+1) + " " + nombres[x]
 
         return mensaje
 
