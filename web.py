e8dc9aac665711e437ce7864eb058bbf03283915
alvarocarmona6 <alvaro.carmona96@gmail.com>
2017-11-20 20:09:10 +0100

    Web

--- web.py
+++ web.py
@@ -22,5 +22,4 @@ def docker():
     return json.dumps(data)
 
 if __name__ == "__main__":
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port, debug = True, use_reloader = True)
+        app.run(debug = True, use_reloader = True)
