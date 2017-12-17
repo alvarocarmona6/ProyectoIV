cd2f51aa1cbe9c04f7e9c639d2543b8cc9a7f62c
alvarocarmona6 <alvaro.carmona96@gmail.com>
2017-12-17 17:52:57 +0100

    ACTUALIZO bot

--- bot/datos.py
+++ bot/datos.py
@@ -6,7 +6,7 @@ import os
 import urlparse
 
 urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+url = urlparse.urlparse(os.environ["postgres://ibsqzhxgjddkrv:e63327f87dab947e1a3499e583d38d5d74eb9b01e14839d3a69e5912688b5285@ec2-54-75-231-85.eu-west-1.compute.amazonaws.com:5432/dcloadl8hlor2p"])
 
 def insertar_jugador(nombre,equipo):
 	conn = psycopg2.connect(database=url.path[1:],user=url.username,password=url.password,host=url.hostname,port=url.port)
