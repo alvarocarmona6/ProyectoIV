f6cd31fa21d0352b367a7bed92adb2f0ff857ef9
alvarocarmona6 <alvaro.carmona96@gmail.com>
2017-12-17 18:48:01 +0100

    ACTUALIZO bot

--- bot/NBAbot.py
+++ bot/NBAbot.py
@@ -6,7 +6,7 @@ import funciones as fun
 import funcionesdatos as fundatos
 import os
 
-TOKEN ='420201411:AAEdz3u2FZmA9kZ9O0cPDyl1YzPO390pw34'
+TOKEN ='420201411:AAGg70ikXnFNWpZt2orrqhQQUt7fsoHuMNA'
 bot = telebot.TeleBot(TOKEN)
 
 def listener(messages): 
